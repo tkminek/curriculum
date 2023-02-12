981efe10423c1fd0569446346c52e97b4072468b
tkminek <kminek.tomas@seznam.cz>
2023-02-12 19:41:15 +0100

    add description

--- my_cv/api/models.py
+++ my_cv/api/models.py
@@ -11,7 +11,7 @@ class Job(models.Model):
     company = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    start_date = models.DateField()
+    start_date = models.CharField(max_length=100)
     end_date = models.CharField(max_length=100)
 
     def __str__(self):
