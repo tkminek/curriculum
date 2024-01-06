8cc7c6c35505397388a678f6ba4c8e13212626be
tkminek <kminek.tomas@seznam.cz>
2024-01-06 16:40:00 +0100

    render.com 6

---
+++ my_cv/frontend/build/precache-manifest.bebb9a2a1d186f073fd420642021e370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ec6169f3ca5736799f1",
+    "url": "/static/css/main.0e74ac91.chunk.css"
+  },
+  {
+    "revision": "8ec6169f3ca5736799f1",
+    "url": "/static/js/main.8ec6169f.chunk.js"
+  },
+  {
+    "revision": "3c4156a189f9562632db",
+    "url": "/static/css/1.ef04d2d4.chunk.css"
+  },
+  {
+    "revision": "3c4156a189f9562632db",
+    "url": "/static/js/1.3c4156a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9379ef50b6867ab1c17ae3b09f0afea7",
+    "url": "/index.html"
+  }
+];
