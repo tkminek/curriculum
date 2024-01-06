b919efe4a604297262d3b3d8ba34804bf9765528
tkminek <kminek.tomas@seznam.cz>
2024-01-06 15:35:25 +0100

    add qunicorn

--- my_cv/my_cv/settings.py
+++ my_cv/my_cv/settings.py
@@ -83,9 +83,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
-        # 'OPTIONS': {
-        #     'deterministic': True,  # Use this only if SQLite >= 3.8.3
-        # },
     },
 }
 
